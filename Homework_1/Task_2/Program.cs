47cd15dd4bcad180b944bfd3e61bcf68621de1d3
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-14 12:35:10 +0600

    ДЗ№1. Задача 4. Max из трех рандомных чисел

---
+++ Homework_1/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int n1 = new Random() .Next(-999, 999);
+Console.WriteLine(n1);
+int n2 = new Random() .Next(-999, 999);
+Console.WriteLine(n2);
+int n3 = new Random() .Next(-999, 999);
+Console.WriteLine(n3);
+int max = n1;
+
+if(n2 > n1)
+{
+    Console.WriteLine($"max = {n2}");
+}
+else
+{
+    Console.WriteLine($"max = {n3}");
+}
+
