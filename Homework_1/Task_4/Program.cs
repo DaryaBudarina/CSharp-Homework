9e432236d67118c7fcc5d07b0578c58da44e2e98
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-14 13:10:43 +0600

    ДЗ№1. Задача 8. Вывод четных чисел

--- Homework_1/Task_4/Program.cs
+++ Homework_1/Task_4/Program.cs
@@ -4,4 +4,15 @@
 8 -> 2, 4, 6, 8
 */
 
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int evenN = 2;
 
+if(n > 1)
+{
+    while(evenN <= n)
+    {
+        Console.Write(evenN + " ");
+        evenN += 2;
+    }
+}
