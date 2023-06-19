a4927d792535d8375661f65229f268d8f3bb7ca2
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-19 12:46:16 +0600

    Исправлена Задача 4.

--- Homework_1/Task_2/Program.cs
+++ Homework_1/Task_2/Program.cs
@@ -20,12 +20,30 @@ Console.Write("Введите число 3: ");
 int n3 = Convert.ToInt32(Console.ReadLine());
 int max = n1;
 
-if(n2 > n1)
+// if(n2 > n1)
+// {
+//    Console.WriteLine($"max = " + Math.Max(n2, n3));
+// }
+// else
+// {
+//        Console.WriteLine($"max = " + Math.Max(n1, n3));
+// }
+
+if(n1 > n2)
 {
-    Console.WriteLine($"max = {n2}");
+    max = n1;
+    if(n1 < n3)
+    {
+        max = n3;
+    }
 }
 else
 {
-    Console.WriteLine($"max = {n3}");
+    max = n2;
+    if(n2 < n3)
+    {
+        max = n3;
+    }
 }
 
+Console.WriteLine($"Max= {max}");
