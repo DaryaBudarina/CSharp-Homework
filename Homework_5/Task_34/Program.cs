d1b6b1de1993d4ee6e7dd10f499ca7d83c7239a9
Darya_Budarina <budarina.d.e@mail.ru>
2023-06-29 20:05:24 +0600

    ДЗ №5: Задачи 34, 36, 38

---
+++ Homework_5/Task_34/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int[] array = CreateArray(4, 100, 999);
+Console.WriteLine($"[{string.Join(", " , array)}]");
+
+int Count(int[]array)
+{
+    int count = 0;
+    foreach(int i in array)
+    {
+        if (i%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int result = Count(array);
+Console.WriteLine($"Количество четных чисел в массиве: {result}");
---
+++ Homework_5/Task_38/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальной и минимальной вещественой частью элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 0.22 - 0.15 = 0.07
+*/
+
+double[] CreateArrayDouble(int size)
+{
+    double[] array = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double d = rand.NextDouble();
+        array[i] = Math.Round(rand.Next(0, 99) + rand.NextDouble(), 2);
+    }
+    return array;
+}
+
+void SearchMaxMin(double[] array, out double min, out double max)
+{
+    min = max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+}
+
+double DifferenceMinMax(double min, double max)
+{
+    return Math.Round(max - min, 2);
+}
+
+Console.Clear();
+System.Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = CreateArrayDouble(size);
+System.Console.WriteLine("[" + string.Join("; ", array) + "]");
+SearchMaxMin(array, out double min, out double max);
+System.Console.WriteLine($" Max = {max}, Min = {min}");
+System.Console.Write($" Разница между максимальной и минимальной вещественой частью элементов массива: {max} - {min} = {DifferenceMinMax(min, max)}");
