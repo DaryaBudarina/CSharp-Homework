53539e59915a4ae696b1ab173755b9548ca6ec6c
Darya_Budarina <budarina.d.e@mail.ru>
2023-07-01 00:47:23 +0600

    ДЗ №6: Задачи 41, 43

---
+++ Homework_6/Task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+int[] Numbers(string text = "Ввести числа через запятую: ")
+{
+    System.Console.Write("Ввести числа через запятую: ");
+    return Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(String.Join(",", array));
+}
+ 
+int NumMoreZero(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum++;
+    }
+    return sum;
+}
+
+int[] array = Numbers();
+PrintArray(array);
+System.Console.WriteLine($"Колличество значений больше 0 = {NumMoreZero(array)}");
---
+++ Homework_6/Task_43/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1*x + b1, y = k2*x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Напр.:b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+double Number(string name)
+{
+    System.Console.Write($"Введите значение {name}");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double pointX(double b2, double b1, double k1, double k2)
+{
+    double point = 0;
+    point = -(b1 - b2) / (k1 - k2);
+    return point;
+}
+
+double pointY(double k1, double x, double b1)
+{
+    double point = 0;
+    point = k1 * x + b1;
+    return point;
+}
+
+double b1 = Number("b1 = ");
+double k1 = Number("k1 = ");
+double b2 = Number("b2 = ");
+double k2 = Number("k2 = ");
+
+Console.WriteLine($"Пересечение в точке: ({pointX(b2, b1, k1, k2)};{pointY(k1, pointX(b2, b1, k1, k2), b1)})");
