14b64c28ced838a1769f2fe1e7fcffbf02596500
Darya_Budarina <budarina.d.e@mail.ru>
2023-07-07 14:36:31 +0600

    ДЗ№7: Задачи 47, 50, 52

---
+++ Homework_7/Task_47/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Clear();
+
+double [,] CreateArrayDouble(int m, int n)
+{ 
+    Random rand = new Random(); 
+    double [,] array = new double [n,m]; 
+    for (int i = 0; i < n; i++) 
+    { 
+        for (int j = 0; j < m; j++) 
+        { 
+            array[i,j] = Math.Round(rand.Next(-9, 10) + rand.NextDouble(), 1); 
+        } 
+    } 
+    return array; 
+} 
+ 
+void PrintArrayDouble (double [,] array)
+{ 
+    int row = array.GetLength(0); 
+    int col = array.GetLength(1); 
+    for (int i = 0; i < row; i++) 
+    { 
+        for (int j = 0; j < col; j++) 
+        { 
+            Console.Write($"{array [i,j]}\t"); 
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+
+System.Console.Write("Ввести количество стобцов n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ввести количество строк m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = CreateArrayDouble(m, n);
+PrintArrayDouble(array);
---
+++ Homework_7/Task_52/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в 
+каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+Console.Clear();
+
+int [,] CreateArray(int m, int n)
+{ 
+    Random rand = new Random(); 
+    int [,] array = new int [n,m]; 
+    for (int i = 0; i < n; i++) 
+    { 
+        for (int j = 0; j < m; j++) 
+        { 
+            array[i,j] = rand.Next(0, 10); 
+        } 
+    } 
+    return array; 
+} 
+ 
+void PrintArray(int [,] array)
+{ 
+    int row = array.GetLength(0); 
+    int col = array.GetLength(1); 
+    for (int i = 0; i < row; i++) 
+    { 
+        for (int j = 0; j < col; j++) 
+        { 
+            Console.Write($"{array [i,j]}\t"); 
+        } 
+        Console.WriteLine(); 
+    } 
+}
+
+double[] GetAverage(int[,] array)
+{
+    int height = array.GetLength(0);
+    int width = array.GetLength(1);
+    double[] average = new double[width];
+    int sum = 0;
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            sum += array[j, i];
+        }
+        average [i] = Math.Round((double)sum / height, 2);
+        sum = 0;
+    }
+    return average;
+}
+
+System.Console.Write("Ввести количество столбцов n => ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ввести количество строк m => ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateArray(m, n);
+PrintArray(array);
+double[] answer = GetAverage(array);
+System.Console.Write($"Среднее арифметическое элементов каждого столбца: {String.Join("; ", answer)}");
