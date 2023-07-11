ddbdf8a2749f910de62a51813040ae8759fb0e0b
Darya_Budarina <budarina.d.e@mail.ru>
2023-07-11 11:40:18 +0600

    ДЗ№8: Задачи 54, 56, 58, 60, 62

---
+++ Homework_8/Task_54/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+двумерного массива. Например, задан массив: 
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив: 
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Clear();
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int GetNumber(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  Random rand = new Random();           
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = rand.Next(1, 10);   
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int m = GetNumber("Введите m: ");
+int n = GetNumber("Введите n: ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"Упорядоченный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
